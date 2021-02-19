46029501b9a62135dd639ce6c09d4456d7d47eb7
Jarrett Cornelison <jcornel00@gmail.com>
2021-02-18 17:32:35 -0800

    Like functionality

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 /**
  * Module dependencies.
  */
@@ -6,7 +5,7 @@
 var express = require('express');
 var http = require('http');
 var path = require('path');
-var handlebars = require('express3-handlebars')
+var handlebars = require('express3-handlebars');
 
 var index = require('./routes/index');
 var project = require('./routes/project');
@@ -37,10 +36,11 @@ if ('development' == app.get('env')) {
 
 // Add routes here
 app.get('/', index.view);
+app.get('/viewAlt', index.viewAlt);
 app.get('/project/:id', project.view);
 // Example route
 // app.get('/users', user.list);
 
-http.createServer(app).listen(app.get('port'), function(){
+http.createServer(app).listen(app.get('port'), function () {
   console.log('Express server listening on port ' + app.get('port'));
 });
--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -1,13 +1,18 @@
 'use strict';
 
 // Call this function when the page loads (the "ready" event)
-$(document).ready(function() {
-	initializePage();
+$(document).ready(function () {
+  initializePage();
 });
 
 /*
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	// your code here
+  $('.likeBtn').click(likeListener);
+}
+
+function likeListener(e) {
+  e.preventDefault();
+  ga('send', 'event', 'like', 'click');
 }
